01f00380d3052b0d42d0e5a109f7bc4074668711
zeFresk <8807862+zeFresk@users.noreply.github.com>
2018-09-30 11:43:07 +0200

    Fixed link errors.
    Working on debug.

--- include/instructions.h
+++ include/instructions.h
@@ -20,21 +20,8 @@ namespace _impl {
 		std::regex reg_params;
 	};
 
-	std::unordered_map<std::string, parameter_data> instructions = {
-	{"INP", { 0, inp_params}},
-	{"OUT", { 1, out_params}},
-	{"LDA", { 2, lda_params}},
-	{ "LDI",{ 3, lda_params } },
-	{"STA", { 4, sta_params}},
-	{ "STI", { 5, sti_params} },
-	{ "ADD",{ 6, add_params }},
-	{ "SUB",{ 7,  sub_params }},
-	{ "JAZ",{ 8, jaz_params }},
-	{ "HRS",{ 9, hrs_params }},
-	{ ".AT", {special_id, at_params}},
-	{ ".WORD",{ special_id, word_params } },
-	{ ".START",{ special_id, at_params } },
-	};
+
+	extern std::unordered_map<std::string, parameter_data> instructions;
 
 }
 
